4b0a137f42ae02fcf591fcc85e092e83bc1a184b
Gerardo J Garcia <gerardojgarcia@pm.me>
2023-02-27 23:46:52 -0800

    fist commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+
+
+    
+
+
+</script>
+
+<template>
+    <div class="border-b-2 border-black w-full py-16">
+        
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+
+import AppHeader from '../components/AppHeader.vue'
+
+
+
+
+
+</script>
+
+
+<template>
+    <div >
+        <div class="container flex border-2 border-black pb-40   m-auto rounded-lg mt-32">
+
+        <AppHeader/>
+        
+        
+        
+        
+        
+        
+        </div>
+    
+        
+    </div>
+</template>
